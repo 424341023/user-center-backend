33403a8b0203ce3b4ef108c5b3444236e750158c
RongGuang <424341023@qq.com>
2024-03-09 10:52:31 +0800

    mybatisX生成代码

---
+++ src/main/java/com/java/usercenterbackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.java.usercenterbackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import generator.domain.User;
+
+/**
+* @author sunshine
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-03-09 10:47:58
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
