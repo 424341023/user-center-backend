c7980be0497deee4b99cca0ac4de191851cbe779
RongGuang <424341023@qq.com>
2024-03-10 11:35:50 +0800

    用户常量定义

---
+++ src/main/java/com/java/usercenterbackend/constant/UserConstant.java
@@ -0,0 +1,29 @@
+package com.java.usercenterbackend.constant;
+
+/**
+ * 用户常量
+ *
+ * @author rongguang
+ * @version 1.0.0
+ * @date 2024年03月10日 11:07:57
+ * @packageName com.java.usercenterbackend.constant
+ * @className UserConstant
+ */
+public interface UserConstant {
+    /**
+     * 用户登录态键
+     */
+    String USER_LOGIN_STATE = "userLoginState:";
+
+    /**
+     * 用户默认权限
+     */
+    int DEFAULT_ROLE = 0;
+
+
+    /**
+     * 管理员默认权限
+     */
+    int ADMIN_ROLE = 1;
+
+}
