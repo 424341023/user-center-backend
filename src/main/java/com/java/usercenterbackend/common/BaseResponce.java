56dd737161e7ac038e47e8e7349251aa8edc65ad
RongGuang <424341023@qq.com>
2024-03-11 18:51:41 +0800

    封装失败响应

--- src/main/java/com/java/usercenterbackend/common/BaseResponce.java
+++ src/main/java/com/java/usercenterbackend/common/BaseResponce.java
@@ -2,6 +2,7 @@ package com.java.usercenterbackend.common;
 
 import lombok.Data;
 
+import javax.xml.stream.events.EndElement;
 import java.io.Serializable;
 
 /**
@@ -21,14 +22,26 @@ public class BaseResponce<T> implements Serializable {
 
     private String message;
 
-    public BaseResponce(int code, T data, String message) {
+    private String description;
+
+    public BaseResponce(int code, T data, String message, String description) {
         this.code = code;
         this.data = data;
         this.message = message;
+        this.description = description;
+    }
+
+    public BaseResponce(int code, T data, String message) {
+        this(code, data, message, "");
     }
 
     public BaseResponce(int code, T data) {
-        this(code, data, "");
+        this(code, data, "", "");
+    }
+
+    public BaseResponce(ErrorCode errorCode){
+        this(errorCode.getCode(), null, errorCode.getMessage(), errorCode.getDescription());
+
     }
 
 }
--- src/main/java/com/java/usercenterbackend/utils/ResultUtils.java
+++ src/main/java/com/java/usercenterbackend/utils/ResultUtils.java
@@ -1,9 +1,11 @@
 package com.java.usercenterbackend.utils;
 
 import com.java.usercenterbackend.common.BaseResponce;
+import com.java.usercenterbackend.common.ErrorCode;
 
 /**
  * 返回工具类
+ *
  * @author rongguang
  * @version 1.0.0
  * @date 2024年03月11日 15:32:03
@@ -11,7 +13,23 @@ import com.java.usercenterbackend.common.BaseResponce;
  * @className ResultUtils
  */
 public class ResultUtils {
-    public static <T> BaseResponce<T> success(T data){
+    /**
+     * 成功
+     * @param data
+     * @return
+     * @param <T>
+     */
+    public static <T> BaseResponce<T> success(T data) {
         return new BaseResponce<>(0, data, "ok");
+    }
+
+    /**
+     * 失败
+     * @param errorCode
+     * @return
+     * @param <T>
+     */
+    public static <T> BaseResponce<T> error(ErrorCode errorCode) {
+        return new BaseResponce<>(errorCode);
     }
 }
