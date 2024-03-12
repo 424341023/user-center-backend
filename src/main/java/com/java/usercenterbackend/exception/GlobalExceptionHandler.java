7a2797705c705e75995d274647242b3c4c15a1cf
RongGuang <424341023@qq.com>
2024-03-12 11:37:42 +0800

    全局异常处理器

---
+++ src/main/java/com/java/usercenterbackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.java.usercenterbackend.exception;
+
+import com.java.usercenterbackend.common.BaseResponce;
+import com.java.usercenterbackend.common.ErrorCode;
+import com.java.usercenterbackend.utils.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author rongguang
+ * @version 1.0.0
+ * @date 2024年03月12日 11:10:20
+ * @packageName com.java.usercenterbackend.exception
+ * @className GlobalExceptionHandler
+ */
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponce bussiessExceptionHandler(BusinessException e) {
+        log.error("businessException: " + e.getMessage(), e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage(), e.getDescription());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponce runtimeExceptionHandler(RuntimeException e) {
+        log.error("runtimeException:" + e.getMessage(), e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage(), "");
+    }
+}
